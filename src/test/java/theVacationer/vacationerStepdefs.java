7f16c71c6a6e29380f21b8a1accf645784485c6e
LAPTOP-1FP83853\Peter <>
2017-07-05 22:12:58 -0400

    Added hotel cucumber test

--- src/test/java/theVacationer/vacationerStepdefs.java
+++ src/test/java/theVacationer/vacationerStepdefs.java
@@ -1,18 +1,22 @@
 package theVacationer;
 
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.springframework.web.client.RestTemplate;
+import theVacationer.model.ApiConnector;
 import theVacationer.model.Header;
 import theVacationer.model.Model;
 import theVacationer.model.landmarks.Landmark;
 import theVacationer.model.landmarks.Places;
 import theVacationer.model.FourSquareApiResponse;
-import theVacationer.model.retaurants.Venue;
+import theVacationer.model.restaurants.Venue;
 
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -38,11 +42,12 @@ public class vacationerStepdefs {
   String city;
   Places pl;
   FourSquareApiResponse resto;
+  FourSquareApiResponse fourSquareApiResponse;
 
   @Given("^A specific location of Paris, France$")
   public void aSpecificLocationOfParisFrance() throws Throwable {
-    String country = "France";
-    String city = "Paris";
+    country = "France";
+    city = "Paris";
     resto = new FourSquareApiResponse(country,city);
   }
 
@@ -114,5 +119,59 @@ public class vacationerStepdefs {
   @Then("^the system should return  \"([^\"]*)\" landmarks$")
   public void theSystemShouldReturnLandmarks(String arg0) throws Throwable {
     assertEquals(Integer.parseInt(arg0),pl.getLandmarkHeaderList().size());
+  }
+
+  @When("^I chose to locate available hotels$")
+  public void iChoseToLocateAvailableHotels() throws Throwable {
+    RestTemplate response = new RestTemplate();
+
+    String baseUrl = "https://api.foursquare.com/v2/venues/search";
+    ApiConnector api = new ApiConnector(baseUrl);
+    Map<String,String> paramMap = new TreeMap<String, String>();
+    paramMap.put("query", "hotel");
+    paramMap.put("limit", "5");
+    paramMap.put("v", "20170701");
+    paramMap.put("client_id", RequestController.CLIENT_ID);
+    paramMap.put("client_secret", RequestController.CLIENT_SECRET);
+    paramMap.put("near", city+","+country);
+    api.setParams(paramMap);
+
+    fourSquareApiResponse = new FourSquareApiResponse(country, city, api);
+  }
+
+  @Then("^there are (\\d+) valid hotels$")
+  public void thereAreValidHotels(int arg0) throws Throwable {
+    fourSquareApiResponse.queryApi();
+    Iterator itr = fourSquareApiResponse.getResponse().getVenues().iterator();
+    while(itr.hasNext()) {
+      Venue venue = (Venue)itr.next();
+      String name = venue.getName();
+      String url = venue.getUrl();
+      assertNotNull(name);
+    }
+  }
+
+  @When("^I chose to locate hotel information$")
+  public void iChoseToLocateHotelInformation() throws Throwable {
+    String baseUrl = "https://api.foursquare.com/v2/venues/search";
+    ApiConnector api = new ApiConnector(baseUrl);
+    Map<String,String> paramMap = new TreeMap<String, String>();
+    paramMap.put("query", "hotel");
+    paramMap.put("limit", "5");
+    paramMap.put("v", "20170701");
+    paramMap.put("client_id", RequestController.CLIENT_ID);
+    paramMap.put("client_secret", RequestController.CLIENT_SECRET);
+    paramMap.put("near", city+","+country);
+    api.setParams(paramMap);
+    fourSquareApiResponse = new FourSquareApiResponse(country, city, api);
+  }
+
+  @Then("^the system should return the top \"([^\"]*)\" hotels$")
+  public void theSystemShouldReturnTheTopHotels(String arg0) throws Throwable {
+    fourSquareApiResponse.queryApi();
+    assertEquals(
+      Integer.parseInt(arg0),
+      fourSquareApiResponse.getResponse().getVenues().size()
+    );
   }
 }
